3949ae5f7514d84b45c9b16ad66fa4157a411d75
Chris Le Sueur <c.m.lesueur@gmail.com>
2014-08-25 23:12:56 +0100

    Fix bug with sounds sometimes not playing shortly after a different sound

--- src/sound.py
+++ src/sound.py
@@ -14,18 +14,20 @@ sounds = {'throw': 'sounds/Throw.wav',
 class SoundPlayer(object):
     def __init__(self):
         self.sounds = {}
+        self.channels = {}
 
         for name, filename in sounds.items():
             self.sounds[name] = pygame.mixer.Sound(filename)
-
-        self.channels = {}
+            self.channels[name] = None
 
     def play(self, name):
         snd = self.sounds[name]
-        if name not in self.channels or not self.channels[name].get_busy():
-            self.channels[name] = snd.play()
-
-        #self.sounds[name].play()
+        if not self.channels[name] or self.channels[name].get_sound() != snd:
+            channel = snd.play()
+            for n in self.channels:
+                if self.channels[n] == channel:
+                    self.channels[n] = None
+            self.channels[name] = channel
 
     def __getattr__(self, name):
         if name in self.sounds:
